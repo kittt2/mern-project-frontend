e21310d58c2c611071ca48ada25a11f68ba2382c
Your Name <you@example.com>
2024-04-19 17:10:12 +0530

    ee'

--- src/pages/USER/Orders.jsx
+++ src/pages/USER/Orders.jsx
@@ -50,7 +50,7 @@ const Orders = () => {
                         <td>{i + 1}</td>
                         <td>{o?.status}</td>
                         <td>{o?.buyer?.name}</td>
-                        <td>{moment(o?.createAt).fromNow()}</td>
+                        <td>{moment(o?.createdAt).fromNow()}</td>
                         <td>{ "Success"}</td>
                         <td>{o?.products?.length}</td>
                       </tr>

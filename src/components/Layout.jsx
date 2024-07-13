c7c3a0f1318f21fc687f48f9118a834f40a315ee
kittt2 <145486768+kittt2@users.noreply.github.com>
2024-07-13 17:40:49 +0530

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
-import React, { useState, useEffect } from "react";
+import  { useState, useEffect } from "react";
 
 import { Outlet } from "react-router-dom";
 import { Toaster } from 'react-hot-toast';

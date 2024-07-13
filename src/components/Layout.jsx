bebb91eddd30269a9b993c184a6b6f9834d57ca7
kittt2 <145486768+kittt2@users.noreply.github.com>
2024-07-13 17:38:28 +0530

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import React, { useState, useEffect } from "react";
+
 import { Outlet } from "react-router-dom";
 import { Toaster } from 'react-hot-toast';
 const Layout = () => {
 
-    
+      
+  useEffect(() => {  
+    toast.custom(<div style={{backgroundColor:"aquamarine",color:"black", height:"50px",borderRadius:"10px",padding:"10px" ,fontWeight:"bolder"}}>"all features need some time to work due to use of free service of render . please wait for 1-2 min. "</div>)
+    console.log("done")
+  }, [])
   return (
     <div>
       
